c1b5ed7c850f6fe5ae84d3f8315093a7e8397f0f
Phil <philanderson888@hotmail.com>
2019-11-28 16:30:25 +0000

    Updating 2019-11-labs

---
+++ Labs/Lab_15_Tasks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lab_15_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // inside can go a DELEGATE OR ANONYMOUS METHOD USING LAMBDA
+            // SYNTAX
+
+            var task01 = new Task(
+                () => { }                  // lambda anonymous method
+            );
+
+            var task02 = new Task(
+                () => { Console.WriteLine("In Task 02"); }                  
+            );
+            task02.Start();
+
+
+
+            // slicker way
+            var task03 = Task.Run(() => { Console.WriteLine("In Task 03"); });
+            var task04 = Task.Run(() => { Console.WriteLine("In Task 04"); });
+            var task05 = Task.Run(() => { Console.WriteLine("In Task 05"); });
+
+            // stopwatch
+            // array of tasks
+            // wait for one to complete / all to complete
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
